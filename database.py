670a4a7328ba7f87ea16dafab1279ea91b16f919
Aditya Jha <adi4ant@gmail.com>
2023-06-08 16:01:04 +0530

    initial commit

---
+++ database.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import seaborn as sns
+
+class Dataset:
+    def __init__(self):
+        self.df = pd.read_csv('data.csv')
+    def plotBar(self,col_name):
+        return sns.countplot(x=col_name,data=self.df)
+    def plotHist(self,col_name):
+        return sns.distplot(self.df[col_name])
+    def plotScatter(self,col_name1,col_name2):
+        return sns.scatterplot(x=col_name1,y=col_name2,data=self.df)
+    def plotBox(self,col_name1,col_name2):
+        return sns.boxplot(x=col_name1,y=col_name2,data=self.df)
